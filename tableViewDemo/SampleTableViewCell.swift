ad894a33f6b7aafa3213ce0fb55e0d939d370f9e
RajaSoftwareLabs <rajasoftwarelabs@RajaSoftwareLabss-Mac-mini.local>
2015-10-06 22:09:22 +0530

    first commit

---
+++ tableViewDemo/SampleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SampleTableViewCell.swift
+//  tableViewDemo
+//
+//  Created by RajaSoftwareLabs on 01/10/15.
+//  Copyright © 2015 jatin. All rights reserved.
+//
+
+import UIKit
+
+class SampleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ tableViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableViewDemo
+//
+//  Created by RajaSoftwareLabs on 01/10/15.
+//  Copyright © 2015 jatin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
